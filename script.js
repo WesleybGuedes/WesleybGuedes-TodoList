1bf099479c0ee1c13de330c75d4773805bb8b1a7
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-04-10 20:41:00 -0300

    feat: requirement 5, 6, 7 implemented

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
+const list = document.querySelector('#lista-tarefas');
 
 const addTask = () => {
-  button.addEventListener('click', () => {
-    const taskList = document.querySelector('#lista-tarefas');
-    const newTask = document.createElement('li');
-    newTask.innerText = input.value;
-    taskList.appendChild(newTask);
+  const taskList = document.querySelector('#lista-tarefas');
+  const newTask = document.createElement('li');
+  newTask.innerText = input.value;
+  taskList.appendChild(newTask);
+  input.value = '';
+};
+
+const changeTaskColor = (event) => {
+  // Garante que nao tera a classe 'selected' em todos os itens da lista
+  const allTasks = document.querySelectorAll('li');
+  allTasks.forEach((task) => {
+    task.classList.remove('selected');
   });
+
+  // Adiciona a classe 'selected' ao item clicado
+  const clickedTask = event.target;
+  clickedTask.classList.add('selected');
+
+  // Define a cor de fundo do item clicado para cinza
+  clickedTask.style.backgroundColor = 'gray';
 };
 
 button.addEventListener('click', addTask);
+list.addEventListener('click', changeTaskColor);
