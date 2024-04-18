0246b2614e341febc55923ad9199f2e278df19b9
WesleybGuedes <wesleyguedes2013@gmail.com>
2024-04-18 18:45:58 -0300

    feat: add saveTasks, moveUp and Down buttons

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const list = document.querySelector('#lista-tarefas');
 const removeSelectedTaskButton = document.querySelector('#remover-selecionado');
 const removeAllTasksButton = document.querySelector('#apaga-tudo');
 const removeCompletedTasksButton = document.querySelector('#remover-finalizados');
+const saveTasksButton = document.querySelector('#salvar-tarefas');
+const moveUpButton = document.querySelector('#mover-cima');
+const moveDownButton = document.querySelector('#mover-baixo');
 
 const addTask = () => {
   const taskList = document.querySelector('#lista-tarefas');
@@ -53,9 +56,39 @@ const removeCompletedTasks = () => {
   });
 };
 
+const moveUp = () => {
+  const allTasks = document.querySelectorAll('li');
+  allTasks.forEach((task) => {
+    if (task.classList.contains('selected')) {
+      task.previousElementSibling.before(task);
+    }
+  });
+};
+
+const moveDown = () => {
+  const allTasks = document.querySelectorAll('li');
+  allTasks.forEach((task) => {
+    if (task.classList.contains('selected')) {
+      task.nextElementSibling.after(task);
+    }
+  });
+};
+
+const saveTasks = () => {
+  const tasks = list.innerHTML;
+  localStorage.setItem('tasks', tasks);
+};
+
+window.onload = () => {
+  list.innerHTML = localStorage.getItem('tasks');
+};
+
 createTaskButton.addEventListener('click', addTask);
 list.addEventListener('click', selectedTask);
 removeSelectedTaskButton.addEventListener('click', removeSelectedTask);
 list.addEventListener('dblclick', completedTask);
 removeAllTasksButton.addEventListener('click', removeAllTasks);
 removeCompletedTasksButton.addEventListener('click', removeCompletedTasks);
+saveTasksButton.addEventListener('click', saveTasks);
+moveUpButton.addEventListener('click', moveUp);
+moveDownButton.addEventListener('click', moveDown);
